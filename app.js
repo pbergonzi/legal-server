0fd03f26e7dc0a8a51c18c8f93680acfd2db53ae
Pablo Jose Bergonzi <pablo.bergonzi@globant.com>
2017-09-09 20:44:23 -0300

    prueba con otro codigo para ipn

--- app.js
+++ app.js
@@ -1,94 +1,114 @@
-const express = require('express')
-const querystring = require('querystring')
+const express = require('express');
+const querystring = require('querystring');
 const request = require('request');
+const colors = require('colors');
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 8080
 
-const app = express()
+colors.setTheme({
+	silly: 'rainbow',
+	input: 'grey',
+	verbose: 'cyan',
+	prompt: 'grey',
+	info: 'green',
+	data: 'grey',
+	help: 'cyan',
+	warn: 'yellow',
+	debug: 'blue',
+	error: 'red'
+});
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+const app = express();
 
-app.listen(port, function () {
-  console.log(`Example app running on port ${port}!`)
-})
+app.use(bodyParser.urlencoded({
+	extended: false
+}))
 
-/**
- * @const {boolean} sandbox Indicates if the sandbox endpoint is used.
- */
-const sandbox = true
+app.get('/', function(req, res) {
+	res.status(200).send("Paypal IPN Listener");
+	res.end('Response will be available on console, nothing to look here!');
+});
 
-/** Production Postback URL */
-const PRODUCTION_VERIFY_URI = "https://ipnpb.paypal.com/cgi-bin/webscr"
-/** Sandbox Postback URL */
-const SANDBOX_VERIFY_URI = "https://ipnpb.sandbox.paypal.com/cgi-bin/webscr"
+app.post('/', function(req, res) {
+	console.log('Received POST /'.bold);
+	console.log(req.body);
+	console.log('\n\n');
 
-/**
- * Determine endpoint to post verification data to.
- * 
- * @return {String}
- */
-const getPaypalURI = () => {
-  return sandbox ? SANDBOX_VERIFY_URI : PRODUCTION_VERIFY_URI
-}
+	// STEP 1: read POST data
+	req.body = req.body || {};
+	res.status(200).send('OK');
+	res.end();
 
-/**
- * @param {Object} req Cloud Function request context for IPN notification event.
- * @param {Object} res Cloud Function response context.
- */
-const ipnHandler = (req, res) => {
-  console.log("IPN Notification Event Received")
-  
-  console.log(req.body)
+	// read the IPN message sent from PayPal and prepend 'cmd=_notify-validate'
+	var postreq = 'cmd=_notify-validate';
+	for (var key in req.body) {
+		if (req.body.hasOwnProperty(key)) {
+			var value = querystring.escape(req.body[key]);
+			postreq = postreq + "&" + key + "=" + value;
+		}
+	}
 
-  if (req.method !== "POST") {
-    console.error("Request method not allowed.")
-    res.status(405).send("Method Not Allowed")
-  } else {
-    // Return empty 200 response to acknowledge IPN post success.
-    console.log("IPN Notification Event received successfully.")
-    res.status(200).end()
-  }
+	// Step 2: POST IPN data back to PayPal to validate
+	console.log('Posting back to paypal'.bold);
+	console.log(postreq);
+	console.log('\n\n');
+	var options = {
+		url: 'https://www.sandbox.paypal.com/cgi-bin/webscr',
+		method: 'POST',
+		headers: {
+			'Connection': 'close'
+		},
+		body: postreq,
+		strictSSL: true,
+		rejectUnauthorized: false,
+		requestCert: true,
+		agent: false
+	};
 
-  // JSON object of the IPN message consisting of transaction details.
-  let ipnTransactionMessage = req.body
-  console.log(ipnTransactionMessage)
-  // Convert JSON ipn data to a query string since Google Cloud Function does not expose raw request data.
-  let formUrlEncodedBody = querystring.stringify(ipnTransactionMessage)
-  // Build the body of the verification post message by prefixing 'cmd=_notify-validate'.
-  let verificationBody = `cmd=_notify-validate&${formUrlEncodedBody}`
+	request(options, function callback(error, response, body) {
+		if (!error && response.statusCode === 200) {
 
-  console.log(`Verifying IPN: ${verificationBody}`)
- 
-  let options = {
-    method: "POST",
-    uri: getPaypalURI(),
-    body: verificationBody,
-  }
+			// inspect IPN validation result and act accordingly
+			if (body.substring(0, 8) === 'VERIFIED') {
+				// The IPN is verified, process it
+				console.log('Verified IPN!'.green);
+				console.log('\n\n');
 
-  // POST verification IPN data to paypal to validate.
-  request(options, function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      // Check the response body for validation results.
-      if (body === "VERIFIED") {
-        console.log(
-          `Verified IPN: IPN message for Transaction ID: ${ipnTransactionMessage.txn_id} is verified.`
-        )
-        // TODO: Implement post verification logic on ipnTransactionMessage
-      } else if (body === "INVALID") {
-        console.error(
-          `Invalid IPN: IPN message for Transaction ID: ${ipnTransactionMessage.txn_id} is invalid.`
-        )
-      } else {
-        console.error("Unexpected reponse body.")
-      }
-    } else {
-      // Error occured while posting to PayPal.
-      console.error(error)
-      console.log(body)
-    }
-  }) 
-}
+				// assign posted variables to local variables
+				var item_name = req.body['item_name'];
+				var item_number = req.body['item_number'];
+				var payment_status = req.body['payment_status'];
+				var payment_amount = req.body['mc_gross'];
+				var payment_currency = req.body['mc_currency'];
+				var txn_id = req.body['txn_id'];
+				var receiver_email = req.body['receiver_email'];
+				var payer_email = req.body['payer_email'];
 
-app.post('/ipn', ipnHandler)
+				//Lets check a variable
+				console.log("Checking variable".bold);
+				console.log("payment_status:", payment_status)
+				console.log('\n\n');
+
+				// IPN message values depend upon the type of notification sent.
+				// To loop through the &_POST array and print the NV pairs to the screen:
+				console.log('Printing all key-value pairs...'.bold)
+				for (var key in req.body) {
+					if (req.body.hasOwnProperty(key)) {
+						var value = req.body[key];
+						console.log(key + "=" + value);
+					}
+				}
+
+			} else if (body.substring(0, 7) === 'INVALID') {
+				// IPN invalid, log for manual investigation
+				console.log('Invalid IPN!'.error);
+				console.log('\n\n');
+			}
+		}
+	});
+});
+
+app.listen(port);
+var msg = 'Listening at http://localhost:' + port;
+console.log(msg.green.bold);
